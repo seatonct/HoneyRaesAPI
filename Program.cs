4f45f4a344ecb5034ef9a45cf21f0db6735fc2a7
Charles Seaton <seatonct@gmail.com>
2024-02-10 16:33:42 -0600

    added Get endpoints for employees and customers and added code to handle requests for data that does not exist

--- Program.cs
+++ Program.cs
@@ -1,3 +1,86 @@
+using HoneyRaesAPI.Models;
+using HoneyRaesAPI.Models.DTOs;
+List<Customer> customers = new List<Customer> 
+{ 
+    new Customer()
+    {
+        Id = 1,
+        Name = "Val Freeman",
+        Address = "128 React Router Dom Dr."
+    },
+    new Customer()
+    {
+        Id = 2,
+        Name = "Steve Brownlee",
+        Address = "12345 Shark Attack Way"
+    },
+    new Customer()
+    {
+        Id = 3,
+        Name = "Jeffrey Lebowski",
+        Address = "12 Golfer Ct"
+    }
+};
+List<Employee> employees = new List<Employee> 
+{ 
+    new Employee()
+    {
+        Id = 1,
+        Name = "Jack Handy",
+        Specialty = "Changin tires"
+    },
+    new Employee()
+    {
+        Id = 2,
+        Name = "Elvin Johnson",
+        Specialty = "navigation systems"
+    }
+};
+List<ServiceTicket> serviceTickets = new List<ServiceTicket> 
+{ 
+    new ServiceTicket()
+    {
+        Id = 1,
+        CustomerId = 1,
+        EmployeeId = 1,
+        Description = "Dropped in toilet",
+        Emergency = true,
+        DateCompleted = new DateTime(2023, 12, 30)
+
+    },
+    new ServiceTicket()
+    {
+        Id = 2,
+        CustomerId = 2,
+        EmployeeId = 1,
+        Description = "cheese whiz in charging port",
+        Emergency = false,
+        DateCompleted = new DateTime(2024, 1, 2)
+    },
+    new ServiceTicket()
+    {
+        Id = 3,
+        CustomerId = 3,
+        EmployeeId = 2,
+        Description = "cracked screen",
+        Emergency = false
+    },
+    new ServiceTicket()
+    {
+        Id = 4,
+        CustomerId = 1,
+        Description = "toilet again",
+        Emergency = false
+    },
+    new ServiceTicket()
+    {
+        Id = 5,
+        CustomerId = 2,
+        Description = "more cheese whiz, now in headphone jack",
+        Emergency = true
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,34 +99,89 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/employees", () => 
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return employees.Select(e => new EmployeeDTO
+    {
+        Id = e.Id,
+        Name = e.Name,
+        Specialty = e.Specialty
+    });
+}
+);
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/employees/{id}", (int id) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    Employee employee = employees.FirstOrDefault(e => e.Id == id);
+    if (employee == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(new EmployeeDTO
+    {
+        Id = employee.Id,
+        Name = employee.Name,
+        Specialty = employee.Specialty
+    });
+});
 
-app.MapGet("/hello", () =>
+app.MapGet("/customers", () => 
 {
-    return "hello";
+    return customers.Select(c => new CustomerDTO
+    {
+        Id = c.Id,
+        Name = c.Name,
+        Address = c.Address
+    });
+}
+);
+
+app.MapGet("/customers/{id}", (int id) =>
+{
+    Customer customer = customers.FirstOrDefault(c => c.Id == id);
+    if (customer == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(new CustomerDTO
+    {
+        Id = customer.Id,
+        Name = customer.Name,
+        Address = customer.Address
+    });
+}
+);
+
+app.MapGet("/servicetickets", () => 
+{
+    return serviceTickets.Select(t => new ServiceTicketDTO
+    {
+        Id = t.Id,
+        CustomerId = t.CustomerId,
+        EmployeeId = t.EmployeeId,
+        Description = t.Description,
+        Emergency = t.Emergency,
+        DateCompleted = t.DateCompleted
+    });
+}
+);
+
+app.MapGet("/servicetickets/{id}", (int id) =>
+{
+    ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+    if (serviceTicket == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(new ServiceTicketDTO
+    {
+        Id = serviceTicket.Id,
+        CustomerId = serviceTicket.CustomerId,
+        EmployeeId = serviceTicket.EmployeeId,
+        Description = serviceTicket.Description,
+        Emergency = serviceTicket.Emergency,
+        DateCompleted = serviceTicket.DateCompleted
+    });
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
