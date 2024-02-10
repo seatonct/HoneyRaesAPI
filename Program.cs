9cb426ba0f6e36415b310cf7a3e2577105a25285
Charles Seaton <seatonct@gmail.com>
2024-02-10 17:14:17 -0600

    used composition to add employee and customer properties on servicetickets and a serviceTickets property on employee and customer GETs by id.

--- Program.cs
+++ Program.cs
@@ -117,11 +117,21 @@ app.MapGet("/employees/{id}", (int id) =>
     {
         return Results.NotFound();
     }
+    List<ServiceTicket> tickets = serviceTickets.Where(st => st.EmployeeId == id).ToList();
     return Results.Ok(new EmployeeDTO
     {
         Id = employee.Id,
         Name = employee.Name,
-        Specialty = employee.Specialty
+        Specialty = employee.Specialty,
+        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        {
+            Id = t.Id,
+            CustomerId = t.CustomerId,
+            EmployeeId = t.EmployeeId,
+            Description = t.Description,
+            Emergency = t.Emergency,
+            DateCompleted = t.DateCompleted
+        }).ToList()
     });
 });
 
@@ -143,11 +153,21 @@ app.MapGet("/customers/{id}", (int id) =>
     {
         return Results.NotFound();
     }
+    List<ServiceTicket> tickets = serviceTickets.Where(st => st.CustomerId == id).ToList();
     return Results.Ok(new CustomerDTO
     {
         Id = customer.Id,
         Name = customer.Name,
-        Address = customer.Address
+        Address = customer.Address,
+        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        {
+            Id = t.Id,
+            CustomerId = t.CustomerId,
+            EmployeeId = t.EmployeeId,
+            Description = t.Description,
+            Emergency = t.Emergency,
+            DateCompleted = t.DateCompleted
+        }).ToList()
     });
 }
 );
@@ -169,15 +189,32 @@ app.MapGet("/servicetickets", () =>
 app.MapGet("/servicetickets/{id}", (int id) =>
 {
     ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+    
     if (serviceTicket == null)
     {
         return Results.NotFound();
     }
+    
+    Employee employee = employees.FirstOrDefault(e => e.Id == serviceTicket.EmployeeId);
+    Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
+    
     return Results.Ok(new ServiceTicketDTO
     {
         Id = serviceTicket.Id,
         CustomerId = serviceTicket.CustomerId,
+        Customer = customer == null ? null : new CustomerDTO
+        {
+            Id = customer.Id,
+            Name = customer.Name,
+            Address = customer.Address
+        },
         EmployeeId = serviceTicket.EmployeeId,
+        Employee = employee == null ? null : new EmployeeDTO
+        {
+            Id = employee.Id,
+            Name = employee.Name,
+            Specialty = employee.Specialty
+        },
         Description = serviceTicket.Description,
         Emergency = serviceTicket.Emergency,
         DateCompleted = serviceTicket.DateCompleted
